ebe0f6e40eaa389803ef74eccdb05f7b4985a1b0
Joel Byler <joelbyler@gmail.com>
2014-08-27 16:13:02 -0400

    Initial Commit

---
+++ WhosNext/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WhosNext
+//
+//  Created by Joel on 8/27/14.
+//  Copyright (c) 2014 LeanDog. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ WhosNext/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  WhosNext
+//
+//  Created by Joel on 8/27/14.
+//  Copyright (c) 2014 LeanDog. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
